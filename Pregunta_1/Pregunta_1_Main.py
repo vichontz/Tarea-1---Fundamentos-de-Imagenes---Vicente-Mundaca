03118a0cc364efe7971f44588998c6f7f1340dbd
vichontz <vicente.mundaca@estudiante.uc.cl>
2026-09-09 18:40:49 -0300

    color sat completa?

--- Pregunta_1/Pregunta_1_Main.py
+++ Pregunta_1/Pregunta_1_Main.py
@@ -49,13 +49,31 @@ def color_saturation( img, puntos, param):
         S_nueva = g(S, m_array, param)              # nota: no hacia falta definir g aparte, pero puede servir en
                                                     # caso de que se quieran hacer otras funciones de forma limpia
         # pasamos a RGB de HSL:
-        skimage.color.hsv2rgb
-        img_saturada = hsl_to_rgb(H, S_nueva, L_val)
-        
-    
+        hsl_mod = np.stack([H, S_nueva, L_val], axis=-1)
+        img_saturada = hsl_to_rgb(hsl_mod)
 
-    return img_saturada
+        return img_saturada
 
-   
+    elif param == "LCH":
+
+        #pasamos a LCH:
+        lch_img = rgb_to_lch(img)
+        L = lch_img[:, :, 0]
+        C = lch_img[:, :, 1]
+        h_val = lch_img[:, :, 2]
+
+        #  Interpolación periódica:
+        m_array = np.interp(h_val, h_ctrl, m_ctrl, period=360.0)
+
+        # Aplicamos g (incluye el recorte [0, 1]).
+        C_nueva = g(C, m_array, param)             
+                                                    
+        # pasamos a RGB de LCH:
+        lch_mod = np.stack([L, C_nueva, h_val], axis=-1)
+        img_saturada = lch_to_rgb(lch_mod)
+
+        return img_saturada
+    else:
+        raise ValueError("El parámetro 'param' debe ser 'HSL' o 'LCH'")
 
 
