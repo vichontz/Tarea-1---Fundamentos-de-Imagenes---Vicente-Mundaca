d47f73b956dc01d08379688dbf787511f3f8c68d
vichontz <vicente.mundaca@estudiante.uc.cl>
2026-09-14 19:09:25 -0300

    p2 función cdfs locales

--- Pregunta_2/Implementacion.py
+++ Pregunta_2/Implementacion.py
@@ -25,4 +25,50 @@ def generar_malla(M, N, Hr, Wr, alpha):
     C = [(y_c, x_c) for y_c in centros_y for x_c in centros_x]
     
     return C, centros_y, centros_x
- 
+
+
+
+
+def calcular_cdfs(img_gris, C, Hr, Wr):
+    cdfs_locales = {}
+    
+    # Estandarización a 8 bits para asegurar 256 bins exactos
+    if img_gris.dtype != np.uint8:
+        if img_gris.max() <= 1.0:
+            img_calc = (img_gris * 255).astype(np.uint8)
+        else:
+            img_calc = img_gris.astype(np.uint8)
+    else:
+        img_calc = img_gris.copy()
+
+    # Iteración sobre centros
+    for y_c, x_c in C:
+    
+        # Definimos las fronteras
+        x_in = x_c - Wr // 2
+        x_fin = x_in + Wr
+        y_in = y_c - Hr // 2
+        y_fin = y_in + Hr
+        
+
+        # Creamos el bloque
+        parche = img_calc[y_in:y_fin, x_in:x_fin]
+        
+        # Cálculo del histograma y la Función de Distribución Acumulada
+        hist, _ = np.histogram(parche.flatten(), bins=256, range=(0, 256))
+        cdf = hist.cumsum()
+        
+        # Normalización de la CDF al rango de intensidades [0, 255]
+        cdf_min = cdf[cdf > 0].min() if cdf.max() > 0 else 0
+        rango_cdf = cdf.max() - cdf_min
+        
+        if rango_cdf > 0:
+            cdf_norm = np.round((cdf - cdf_min) * 255 / rango_cdf).astype(np.uint8)
+        else:
+            # Caso borde (un solo color)
+            cdf_norm = np.arange(256, dtype=np.uint8)
+            
+        # Diccionario usando el centro como key
+        cdfs_locales[(y_c, x_c)] = cdf_norm
+        
+    return cdfs_locales, img_calc
