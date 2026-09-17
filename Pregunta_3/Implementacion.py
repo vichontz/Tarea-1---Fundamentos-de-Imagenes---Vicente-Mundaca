b3d342894976909bf37e4aa499c453c88efc0e46
vichontz <vicente.mundaca@estudiante.uc.cl>
2026-09-17 17:59:53 -0300

    unpixel

--- Pregunta_3/Implementacion.py
+++ Pregunta_3/Implementacion.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from skimage import data 
 def reescalar_imagen(img, s, modo="bilineal"):
     H_in, W_in = img.shape[:2]
 
@@ -59,4 +59,42 @@ def reescalar_imagen(img, s, modo="bilineal"):
 
     # Recorte final y restauración del tipo de dato original
     salida = np.clip(salida, 0, 255)
-    return salida.astype(img.dtype)
+    return np.round(salida).astype(img.dtype)
+
+
+#Testeo para un pixel:
+img = data.camera() #imagen random
+s = 1.37
+H_in, W_in = img.shape
+
+i, j = 200, 340
+
+y = i / s
+x = j / s
+y0, x0 = int(np.floor(y)), int(np.floor(x))
+y1, x1 = y0 + 1, x0 + 1
+wy, wx = y - y0, x - x0
+
+y0c, y1c = min(y0, H_in-1), min(y1, H_in-1)
+x0c, x1c = min(x0, W_in-1), min(x1, W_in-1)
+
+p00 = float(img[y0c, x0c]); p01 = float(img[y0c, x1c])
+p10 = float(img[y1c, x0c]); p11 = float(img[y1c, x1c])
+
+arriba = p00*(1-wx) + p01*wx
+abajo  = p10*(1-wx) + p11*wx
+valor  = arriba*(1-wy) + abajo*wy
+
+out = reescalar_imagen(img, s, modo='bilineal')
+
+print(f'Trazado de un pixel, s={s}, modo bilineal')
+print(f'Coordenada salida           : (i,j) = ({i}, {j})')
+print(f'Coordenada entrada          : (y,x) = ({y:.3f}, {x:.3f})')
+print(f'Vecino arriba-izq           : ({y0},{x0}) = {p00}')
+print(f'Vecino arriba-der           : ({y0},{x1}) = {p01}')
+print(f'Vecino abajo-izq            : ({y1},{x0}) = {p10}')
+print(f'Vecino abajo-der            : ({y1},{x1}) = {p11}')
+print(f'Pesos                       : wy = {wy:.3f}, wx = {wx:.3f}')
+print(f'Interpolacion horizontal    : arriba = {arriba:.3f}, abajo = {abajo:.3f}')
+print(f'Valor interpolado final     : {valor:.3f} redondeado = {int(np.clip(round(valor),0,255))}')
+print(f'Valor real [{i},{j}]: {out[i,j]} ')
